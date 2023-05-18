132d2e41ef851ed5a494a4139b9feaeb4f7632f6
Soham <asoham405@gmail.com>
2023-05-18 23:09:43 +0530

    Added components folder, learned about props and adding bootstrap to react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
-let heading="Hello World!"
 function App() {
   return (
-  <div className="main">
-    <h1>{heading}</h1>
-    <p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis eveniet corporis ipsum soluta nostrum debitis, aliquid aliquam dolore placeat quis doloribus, ullam aspernatur vel cumque officiis. Enim cupiditate consequuntur magni architecto reprehenderit exercitationem nemo. Fugit in saepe consequuntur delectus deleniti ipsam reiciendis! Alias molestias perspiciatis dolorum eum blanditiis quod praesentium.
-    </p>
-  </div>
+  <>
+    <Navbar title="TextUtils"/>
+  </>
   );
 }
 
