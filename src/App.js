c14305846338f4d4efc8c81f992627783d4eb82f
Soham <asoham405@gmail.com>
2023-05-18 21:14:45 +0530

    added paragraph

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
+let heading="Hello World!"
 function App() {
   return (
   <div className="main">
-    <h1>Heading</h1>
+    <h1>{heading}</h1>
+    <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis eveniet corporis ipsum soluta nostrum debitis, aliquid aliquam dolore placeat quis doloribus, ullam aspernatur vel cumque officiis. Enim cupiditate consequuntur magni architecto reprehenderit exercitationem nemo. Fugit in saepe consequuntur delectus deleniti ipsam reiciendis! Alias molestias perspiciatis dolorum eum blanditiis quod praesentium.
+    </p>
   </div>
   );
 }
