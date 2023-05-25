a7f023eeab5fe143fae2e383864ec44036620574
Soham <asoham405@gmail.com>
2023-05-25 13:48:05 +0530

    Corrected icon issue (added some icons and made corrections in manifest.json accordingly Added react router and implemented it to switch between home and about.  Issue Unresolved: On switching dark mode type , button colors switch as intented, but background color fails to change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
 import "./App.css";
 import Alert from "./Components/Alert";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
-// import About from "./Components/About";
+import About from "./Components/About";
 
 function App() {
   const [mode, setMode] = useState("dark");
@@ -11,7 +17,7 @@ function App() {
     type: null,
     message: null,
   });
-  const [buttonColor,setButtonColor] = useState("primary")
+  const [buttonColor, setButtonColor] = useState("primary");
 
   document.body.style.backgroundColor = `${
     mode === "dark" ? "#2e3d4d" : "white"
@@ -22,51 +28,82 @@ function App() {
     if (mode === "light") {
       setMode("dark");
       showAlert("dark", "Dark Mode Enabled");
-      document.title = "Text Util - Dark Mode"
+      document.title = "Text Util - Dark Mode";
     } else {
       setMode("light");
       showAlert("light", "Light Mode Enabled");
-      document.title = "Text Util - Light Mode"
+      document.title = "Text Util - Light Mode";
     }
-    setButtonColor("primary")
+    setButtonColor("primary");
   };
 
-  var myTimeout=null
+  var myTimeout = null;
   const showAlert = (type, message) => {
     setAlert({
       type: type,
       message: message,
     });
     clearTimeout(myTimeout);
-    myTimeout=setTimeout(() => {
+    myTimeout = setTimeout(() => {
       setAlert({
         type: null,
       });
     }, 2000);
   };
 
-  const changegreen = ()=>{
-    document.body.style.backgroundColor = '#13521f'
-    setButtonColor("success")
-  }
-  const changeblue = ()=>{
-    document.body.style.backgroundColor = "#2e3d4d"
-    setButtonColor("primary")
-  }
-  const changered = ()=>{
-    document.body.style.backgroundColor = '#621919'
-    setButtonColor("danger")
-  }
+  const changegreen = () => {
+    document.body.style.backgroundColor = "#13521f";
+    setButtonColor("success");
+  };
+  const changeblue = () => {
+    document.body.style.backgroundColor = "#2e3d4d";
+    setButtonColor("primary");
+  };
+  const changered = () => {
+    document.body.style.backgroundColor = "#621919";
+    setButtonColor("danger");
+  };
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} changegreen={changegreen} changered={changered} changeblue={changeblue}/>
-      <div className="container-fluid z-2 position-absolute">
-        <Alert alert={alert} />
-      </div>
-      <div className="container my-3">
-        <TextForm heading="Enter your Text" mode={mode} showAlert={showAlert} buttonColor={buttonColor} />
-        {/* <About/> */}
-      </div>
+      {/* Rourter */}
+      <Router>
+        <Navbar
+          title="TextUtils"
+          mode={mode}
+          toggleMode={toggleMode}
+          changegreen={changegreen}
+          changered={changered}
+          changeblue={changeblue}
+        />
+        <div className="container-fluid z-2 position-absolute">
+          <Alert alert={alert} />
+        </div>
+
+        <Routes>
+          <Route
+            path="/about"
+            element={
+              <div className="container my-3">
+                <About />
+              </div>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <div className="container my-3">
+                <TextForm
+                  heading="Enter your Text"
+                  mode={mode}
+                  showAlert={showAlert}
+                  buttonColor={buttonColor}
+                />
+              </div>
+            }
+          />
+        </Routes>
+      </Router>
+      {/* Rourter - END */}
     </>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   let changebackground;
@@ -16,33 +17,34 @@ export default function Navbar(props) {
         </button>
         <ul className="dropdown-menu">
           <li>
-            <a className="dropdown-item" onClick={props.changegreen} href="#">
+            <Link className="dropdown-item" onClick={props.changegreen} to="">
               Green
-            </a>
+            </Link>
           </li>
           <li>
-            <a className="dropdown-item" onClick={props.changered} href="#">
+            <Link className="dropdown-item" onClick={props.changered} to="">
               Red
-            </a>
+            </Link>
           </li>
           <li>
-            <a className="dropdown-item" onClick={props.changeblue} href="#">
+            <Link className="dropdown-item" onClick={props.changeblue} to="">
               Blue
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
     );
   }
+
   return (
     <nav
       className={`navbar navbar-expand-lg bg-body-secondary bg-${props.mode}`}
       data-bs-theme={props.mode}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to='/'>
           {props.title}
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -57,14 +59,14 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <Link className="nav-link active" aria-current="page" to='/'>
                 {props.home}
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <Link className="nav-link" to='/about'>
                 {props.about}
-              </a>
+              </Link>
             </li>
           </ul>
           {changebackground}
